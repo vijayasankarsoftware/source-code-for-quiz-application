ae97b858d11501223a2e29248e1a6d866b5d4a8e
vijayasankarsoftware <142673449+vijayasankarsoftware@users.noreply.github.com>
2023-09-15 22:06:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function Subscribe() {
+   
+    var d = document.getElementById("Downloadbtn");
+    link = "https://www.youtube.com/channel/UCnShImWEdbCUkDa3uUEt-IA/about"
+    window.location.href = link;
+    d.style.display = "block";
+}
+
+function download() {
+    var d = document.getElementById("downloadlink");
+    d.click()
+}
+
+
